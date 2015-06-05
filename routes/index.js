380ab3637949e7f655ed3d7209c4cd7683b750e4
Virginia Redondo <virginia.redondoanton@gmail.com>
2015-06-05 05:10:02 -0600

    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 // GET /author
 router.get('/author',   function(req, res) {
