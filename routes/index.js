8b0ae27a0517f8b9b930086e788a02e17f5d0ceb
Virginia Redondo <virginia.redondoanton@gmail.com>
2015-06-22 12:42:57 -0600

    Crear comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -21,9 +22,12 @@ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
++router.post('/quizes/:quizId(\\d+)/comments',   commentController.create);
+
 // GET /author
 router.get('/author',   function(req, res) {
-  res.render('author', { title: 'Author' });
+  res.render('author', { title: 'Author', errors: [] });
 });
 
 module.exports = router;
